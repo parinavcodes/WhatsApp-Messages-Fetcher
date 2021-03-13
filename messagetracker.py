e069d1bc8812514ff89d1f9a08741004f6953bd7
Parinav <parinavputhran@gmail.com>
2021-03-13 16:53:14 +0530

    need to configure listener for new messages

--- messagetracker.py
+++ messagetracker.py
@@ -98,7 +98,31 @@ def data_fetcher(messages_detail):
                 print(message.text, detail.get_attribute("data-pre-plain-text"))
                 f.write(user + "," + message_date + "," + message_time + "," + message.text + "," + "\n")
 
-def whatsapp_data_fetcher(driver,group_name,*args,**kwargs):
+def whatsapp_data_fetcher(driver,group_select):
+    t_out = 5
+    group_select.click()
+    try:
+        element_pres = EC.presence_of_element_located((By.XPATH,
+                                                       '//div[contains(@class,"copyable-text")]/div[@class="_3ExzF"]/span[contains(@class,"copyable-text")]'))
+        WebDriverWait(driver, t_out).until(element_pres)
+        # messages_tab = driver.find_element_by_xpath('//div[@class="_11liR"]')
+        messages_details = driver.find_elements_by_xpath(
+            '//div[@class="_1bR5a"]/div[contains(@class,"copyable-text")]')
+        # messages = driver.find_elements_by_xpath(
+        #     '//div[@class="copyable-text"]/div[@class="_3ExzF"]/span[contains(@class,"copyable-text")]')
+
+        if not os.path.isfile("./messagedata.csv"):
+            init_time_top = messages_details[0].get_attribute("data-pre-plain-text").split("[")[1].split(",")[0]
+            scroller(init_time_top, "top")
+            with open("messagedata.csv", 'a') as f:
+                f.write("User,Date,Time,Message\n")
+
+        data_fetcher(messages_details)
+    except TimeoutException:
+        print("field not found")
+
+
+def group_finder(driver,group_name,*args,**kwargs):
     t_out = 20
     try:
         element_pres = EC.presence_of_element_located((By.XPATH, '//div[@title="Menu"]'))
@@ -106,10 +130,41 @@ def whatsapp_data_fetcher(driver,group_name,*args,**kwargs):
         # print("found")
         group_select = driver.find_element_by_xpath("//span[@title=" + group_name + "]")
         # print(group_select)
-        group_select.click()
+    except TimeoutException:
+        print("User not logged in")
 
+class MessageFetcher():
+    def __init__(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        url = "http://web.whatsapp.com/"
+        self.group_name = "\"Test group\""
+        self.group_select=None
+        # driver = EventFiringWebDriver(driver, MessageListener())
+        driver = EventFiringWebDriver(driver, MessageListener(group_name))
+        driver.get(url)
+        # driver.execute_script(
+        #     "var options = { childList:true }; const observer = new MutationObserver(arguments[1]); observer.observe(arguments[0],options);",
+        #     driver.find_element_by_xpath('//div[@class="_2aBzC"]'), whatsapp_data_fetcher)
+
+        self.group_finder(driver, group_name)
+        # driver.back()
+        # with Listener(on_press=activecheck) as listener:
+        #     listener.join()
+
+    def group_finder(self,driver, group_name, *args, **kwargs):
+        t_out = 20
+        try:
+            element_pres = EC.presence_of_element_located((By.XPATH, '//div[@title="Menu"]'))
+            WebDriverWait(driver, t_out).until(element_pres)
+            # print("found")
+            self.group_select = driver.find_element_by_xpath("//span[@title=" + group_name + "]")
+            # print(group_select)
+        except TimeoutException:
+            print("User not logged in")
+
+    def whatsapp_data_fetcher(self,driver, group_select):
         t_out = 5
-
+        group_select.click()
         try:
             element_pres = EC.presence_of_element_located((By.XPATH,
                                                            '//div[contains(@class,"copyable-text")]/div[@class="_3ExzF"]/span[contains(@class,"copyable-text")]'))
@@ -122,27 +177,84 @@ def whatsapp_data_fetcher(driver,group_name,*args,**kwargs):
 
             if not os.path.isfile("./messagedata.csv"):
                 init_time_top = messages_details[0].get_attribute("data-pre-plain-text").split("[")[1].split(",")[0]
-                scroller(init_time_top, "top")
+                self.scroller(init_time_top, "top")
                 with open("messagedata.csv", 'a') as f:
                     f.write("User,Date,Time,Message\n")
 
-            data_fetcher(messages_details)
+            self.data_fetcher(messages_details)
         except TimeoutException:
             print("field not found")
 
-    except TimeoutException:
-        print("User not logged in")
+    def data_fetcher(self,messages_detail):
+        init_time_bottom =messages_detail[len(messages_detail) - 1].get_attribute("data-pre-plain-text").split("[")[1].split(",")[0]
+        self.scroller(init_time_bottom, "bottom")
 
-class MessageListener(AbstractEventListener):
+        messages_details = driver.find_elements_by_xpath('//div[@class="_1bR5a"]/div[contains(@class,"copyable-text")]')
+        messages = driver.find_elements_by_xpath(
+            '//div[contains(@class,"copyable-text")]/div[@class="_3ExzF"]/span[contains(@class,"copyable-text")]')
+
+        latest_row = ""
+        with open("messagedata.csv", 'r') as f:
+            for row, i in zip(reversed(list(csv.reader(f))), range(0, 1)):
+                latest_row = (",".join(row))
+
+        latest_date = latest_row.split(",")[1]
+        latest_time = latest_row.split(",")[2]
+
+        with open("messagedata.csv", 'a+') as f:
+            for message, detail in zip(messages, messages_details):
+                message_time = detail.get_attribute("data-pre-plain-text").split("[")[1].split(",")[0]
+                message_date = detail.get_attribute("data-pre-plain-text").split(", ")[1].split("]")[0]
+                user = detail.get_attribute("data-pre-plain-text").split("] ")[1].split(":")[0]
+                # print(datetime.datetime.strptime(message_time, '%H:%M'), datetime.datetime.strptime(latest_time, '%H:%M'),datetime.datetime.strptime(latest_date, '%d/%m/%Y'), datetime.datetime.strptime(message_date,'%d/%m/%Y'))
+                # print(message_date,message_time,latest_date,latest_time)
+                if latest_date == "Date":
+                    print(message.text, detail.get_attribute("data-pre-plain-text"))
+                    f.write(user + "," + message_date + "," + message_time + "," + message.text + "," + "\n")
+
+                elif datetime.datetime.strptime(latest_date, "%d/%m/%Y") <= datetime.datetime.strptime(message_date,
+                                                                                                       "%d/%m/%Y") and datetime.datetime.strptime(
+                    message_time, "%H:%M") > datetime.datetime.strptime(latest_time, "%H:%M"):
+                    print(message.text, detail.get_attribute("data-pre-plain-text"))
+                    f.write(user + "," + message_date + "," + message_time + "," + message.text + "," + "\n")
+
+    def scroller(self,init_time, dir):
+        item = 0
+        message_tab = driver.find_element_by_class_name('_11liR')
+        message_tab.click()
+        if dir == "top":
+            # time.sleep(3)
+            message_tab.send_keys(Keys.CONTROL + Keys.HOME)
+            time.sleep(4)
+            message_tab.send_keys(Keys.CONTROL + Keys.HOME)
+        else:
+            message_tab.send_keys(Keys.CONTROL + Keys.END)
+            time.sleep(1)
+            message_tab.send_keys(Keys.CONTROL + Keys.END)
+        # item = len(messages_details) - 1
+
+        message_details = driver.find_elements_by_xpath('//div[@class="_1bR5a"]/div[contains(@class,"copyable-text")]')
+        # # driver.execute_script("arguments[0].scrollIntoView();", messages_details[item])
+        item = 0 if dir == "top" else len(message_details) - 1
+        message_time = message_details[item].get_attribute("data-pre-plain-text").split("[")[1].split(",")[0]
+        # print(dir)
+        # time.sleep(2)
+        # print(datetime.datetime.strptime(message_time, '%H:%M'), datetime.datetime.strptime(init_time, '%H:%M'))
+        if datetime.datetime.strptime(message_time, '%H:%M') == datetime.datetime.strptime(init_time, '%H:%M'):
+            return
+        scroller(message_time, dir)
+
+
+class MessageListener(AbstractEventListener,MessageFetcher):
     def __init__(self,group_name):
         self.group_name=group_name
 
-    def before_click(self, element, driver):
+    def before_find(self, element, driver):
         element=element.find_element_by_xpath("./../../..").find_element_by_xpath('//div[@class="_1SjZ2"]/div[@class="_15smv"]')
         print(element.get_attribute("class"))
         if len(element.text)!=0:
             print("before_click %s" % element.get_attribute("class"))
-            whatsapp_data_fetcher(driver, self.group_name)
+            super().whatsapp_data_fetcher(driver, self.group_select)
 
     # def before_change_value_of(self, element, driver):
     #     print("before_change_value_of")
@@ -153,18 +265,18 @@ def activecheck(*args):
     print(args[0])
     return False if args[0] == Key.esc else True
 if __name__=="__main__":
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    url = "http://web.whatsapp.com/"
-    group_name = "\"Test group\""
-
-    # driver = EventFiringWebDriver(driver, MessageListener())
-    driver = EventFiringWebDriver(driver, MessageListener(group_name))
-    driver.get(url)
-    # driver.execute_script(
-    #     "var options = { childList:true }; const observer = new MutationObserver(arguments[1]); observer.observe(arguments[0],options);",
-    #     driver.find_element_by_xpath('//div[@class="_2aBzC"]'), whatsapp_data_fetcher)
-
-    whatsapp_data_fetcher(driver,group_name)
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
+    # url = "http://web.whatsapp.com/"
+    # group_name = "\"Test group\""
+    #
+    # # driver = EventFiringWebDriver(driver, MessageListener())
+    # driver = EventFiringWebDriver(driver, MessageListener(group_name))
+    # driver.get(url)
+    # # driver.execute_script(
+    # #     "var options = { childList:true }; const observer = new MutationObserver(arguments[1]); observer.observe(arguments[0],options);",
+    # #     driver.find_element_by_xpath('//div[@class="_2aBzC"]'), whatsapp_data_fetcher)
+    messageFetcher=MessageFetcher()
+    messageFetcher.group_finder(driver,group_name)
     # driver.back()
     # with Listener(on_press=activecheck) as listener:
     #     listener.join()
